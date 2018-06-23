ae8b60811a267fc79eb9ced23b19884f71682e3b
jacobsilver2 <jacobsilver2@mac.com>
2018-06-23 16:29:14 -0400

    added user authentication seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621163612) do
+ActiveRecord::Schema.define(version: 20180623202527) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
